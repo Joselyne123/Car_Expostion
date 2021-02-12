b07b11591a8f7e79502a5d06bc382df1f7c7518e
joseingabire123@gmail.com <joseingabire123@gmail.com>
2021-02-12 09:14:32 +0200

    changes

--- carhubpro/settings.py
+++ carhubpro/settings.py
@@ -141,11 +141,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Email configurations
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'joseingabire123@gmail.com'
-EMAIL_HOST_PASSWORD = '0725517642'
+# EMAIL_USE_TLS = True
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = 'joseingabire123@gmail.com'
+# EMAIL_HOST_PASSWORD = '0725517642'
 
 
 
